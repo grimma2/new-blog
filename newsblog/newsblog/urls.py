25cfb8b3a175bca030221c0ff33a5999ba0710d7
grimma2 <chella05andrey@gmail.com>
2025-07-03 01:00:20 +0300

    ю

--- newsblog/newsblog/urls.py
+++ newsblog/newsblog/urls.py
@@ -27,6 +27,6 @@ urlpatterns = [
     path('api/', include('news.urls')),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+# Статические и медиа файлы (для development и production)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
